44afa1dc017ded70a09ad4640f91e310db95e818
Hilal <72453298+Hilal-Almoqbali@users.noreply.github.com>
2022-05-30 18:02:36 +0000

    changes

--- server/GS/main.cc
+++ server/GS/main.cc
@@ -1,6 +1,7 @@
 #include <czmq.h>
 int main (void)
 {
+    
     zsock_t *push = zsock_new_push ("inproc://example");
     zsock_t *pull = zsock_new_pull ("inproc://example");
     zstr_send (push, "Hello, World");
