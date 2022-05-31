ecd5cc5cbe7f968e0ce2b8ff05bfa678b55e54d4
Hilal <72453298+Hilal-Almoqbali@users.noreply.github.com>
2022-05-31 01:59:00 +0000

    changes

--- server/match maker/MMS.cc
+++ server/match maker/MMS.cc
@@ -4,23 +4,16 @@
 #include <string.h>
 #include <assert.h>
 
+char* readFile(char* filename);
+
 int main (void)
 {
+    printf("%s","start\n");
     // pointer demo to FILE
-    FILE* myport;
-    int display;
- 
-    // Creates a file "demo_file"
-    // with file access as read mode
-    myport = fopen("myport.txt", "r");
-    char array[20];
-    int results = fputs(array, myport);
-    if (results == EOF) 
-    {
-    // Failed to write do error code here.
-    }
-    fclose(myport);
-    printf("%s",array);}
+
+    char *port = readFile("myport.txt");
+    
+}
 /*
     //  Socket to talk to clients
     void *context = zmq_ctx_new ();
@@ -50,3 +43,28 @@ int main (void)
     // with file access as read mode
     demo = fopen("demo_file.txt", "r");
     */
+
+
+
+    
+
+
+    
+char* readFile(char* filename)
+{
+    FILE* file = fopen(filename,"r");
+    if(file == NULL)
+    {
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long int size = ftell(file);
+    rewind(file);
+
+    char* content = calloc(size + 1, 1);
+
+    fread(content,1,size,file);
+
+    return content;
+}
